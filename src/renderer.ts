8417d795f27c6bc33f933bf4ab7c445998a335dc
Syphaxtouadies <syphaxtouadii@gmail.com>
2026-09-21 00:10:46 +0100

    Update task system

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,12 +26,68 @@
  * ```
  */
 
+import { stat } from 'original-fs';
 import './index.css';
 
 const OpenSettings = document.getElementById("OpenSettings");
 const BackToMain = document.getElementById("BackToMain");
 const settingsscreen = document.getElementById("settingsScreen");
 const mainscreen = document.getElementById("mainScreen");
+const taskList = document.getElementById("taskList");
+const input = document.getElementById("taskInput") as HTMLInputElement;
+const AddTaskButton = document.getElementById("AddTask");
+const CompletedLabel = document.getElementById("Completed")
+const TasksLabel = document.getElementById("Tasks")
+
+let currentidx = 0
+let taskamount = 0
+let completedamount = 0
+
+const usedTexts: Record<string, boolean> = {};
+
+function addTask(text: string): any {
+    const task = document.createElement("div");
+
+    if (usedTexts[text] || text.length <= 0) {
+      return undefined;
+    };
+
+    usedTexts[text] = true
+
+    currentidx += 1;
+    taskamount += 1;
+    updatelabels()
+
+    task.innerHTML = `
+        <input type="checkbox" id="id_${currentidx}">
+        <label class="tasklabel">${text}</label>
+    `;
+
+    const taskinput = task.querySelector("input")
+
+    function updatelabels(){
+        if (TasksLabel && CompletedLabel) {
+          TasksLabel.textContent = "Tasks: " + taskamount;
+          CompletedLabel.textContent = "Completed: " + completedamount;
+        }
+    }
+    
+    task.addEventListener("change", () => {
+      const state = task as HTMLInputElement;
+
+      console.log(taskinput?.checked);
+
+      if (taskinput?.checked) {
+          completedamount += 1;
+      } else {
+          completedamount -= 1;
+      }
+
+      updatelabels();
+    }); 
+
+    taskList?.appendChild(task);
+}
 
 OpenSettings?.addEventListener("click", () => {
   console.log("Clicked");
@@ -43,6 +99,10 @@ OpenSettings?.addEventListener("click", () => {
 BackToMain?.addEventListener("click", () => {
   mainscreen?.removeAttribute("hidden");
   settingsscreen?.setAttribute("hidden", "")
+})
+
+AddTaskButton?.addEventListener("click", () => {
+  addTask(input.value)
 })
 
 console.log("hello yellow verity")
